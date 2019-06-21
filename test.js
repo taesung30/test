d2a3ae1d059e2e9c641e947cb9c0efeaf716ee8b
taesung30 <51192631+taesung30@users.noreply.github.com>
2019-06-21 16:18:45 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,14 @@
+var aryNum = [2, 413, 32, 55, 21];
+aryNum.sort(function (a, b) {
+  return a - b;
+});
+
+for (var i = 0; i < aryNum.length; i++) {
+  document.writeln(aryNum[i]);
+  //console.log('test ' + aryNum[i]);
+}
+
+
+
+
+document.writeln('Start!');
